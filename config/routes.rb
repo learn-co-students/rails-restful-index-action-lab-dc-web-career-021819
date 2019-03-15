8c60d9b36191ab11c80148febd9bb0d7ab27030f
Ammar Ali <funnyguy118@gmail.com>
2019-03-15 00:29:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # the route should be /students, mapped to a students' controller index action,
+  resources :students, only: :index
 end
